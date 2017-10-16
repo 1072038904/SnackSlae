858ab942b38dbb644cad68afeaeb251efdfd0550
1072038904 <1072038904@qq.com>
2017-10-16 22:32:40 +0800

    所有后台底层完成

--- src/com/model/Account.java
+++ src/com/model/Account.java
@@ -1,41 +1,25 @@
 package com.model;
 
 
-import java.io.Serializable;
-
 import javax.persistence.*;
 @Entity
-public class Account implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2046045738166618185L;
-	@Id@Column(name="account_id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
+public class Account {
+	@Id
 	private  Integer account;
 	private String password;
 	private Integer jurisdiction;
-	public Integer getId() {
-		return id;
-	}
 	public Integer getJurisdiction() {
 		return jurisdiction;
 	}
 	public void setJurisdiction(Integer jurisdiction) {
 		this.jurisdiction = jurisdiction;
 	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
 	public Integer getAccount() {
 		return account;
 	}
 	public void setAccount(Integer account) {
 		this.account = account;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 	public String getPassword() {
 		return password;
